b47cd29789bb165d1937116541ce380085d73e99
Christophe Lebel <christophe_lebel@videotron.ca>
2020-08-14 11:12:21 -0400

    servitude modifications

--- src/steering-servitude.cpp
+++ src/steering-servitude.cpp
@@ -14,7 +14,12 @@
 #include "std_msgs/Float64.h"
 #include "std_msgs/Bool.h"
 #include "can_msgs.h"
-const double KP = 1850, KI = 8.3, KD = 1900, DIV = 10000;
+const double KIA_SOUL_ACCELERATION = 4.34; // m/s2
+const double MAX_ACCELARATION = 1.0; // m/s2
+const double MAX_DECELARATION = 1.0; // m/s2
+const double KIA_SOUL_DECELERATION = 8; // m/s2
+const double SPEED_KP = 1850, SPEED_KI = 8.3, SPEED_KD = 1900, SPEED_DIV = (10000 * KIA_SOUL_ACCELERATION);
+const double STEERING_KP = 0, STEERING_KI = 0, STEERING_KD = 0, STEERING_DIV = 0;
 
 double steering = 0, speed = 0, steering_actual = 0, speed_actual = 0;
 ros::Publisher pub_torque;
@@ -75,29 +80,40 @@ int main(int argc, char *argv[])
     pub_acceleration = n.advertise<std_msgs::Float64>("/car/throttle", 1);
     pub_brake = n.advertise<std_msgs::Float64>("/car/brake", 1);
     ros::Rate rate(2);
-    double sum_error = 0;
-    double previous_error = 0;
+    double sum_error_speed = 0;
+    double previous_error_speed = 0;
+    double sum_error_steering = 0;
+    double previous_error_steering = 0;
 
     while(ros::ok())
     {
         ros::spinOnce();
         rate.sleep();
-        //replace with servitude
+
+        double steering_error = steering_actual - steering;
+        sum_error_steering += steering_error;
+        double steering_delta_error = steering_error-previous_error_steering;
+        previous_error_steering = steering_error;
+        double cmd = (STEERING_KP*steering_error + STEERING_KI*sum_error_steering + STEERING_KD*steering_delta_error) / STEERING_DIV;
+        if(cmd < 0) cmd=0;
+        else if(cmd > 255) cmd = 255;
+
         double torque = steering;
 
         auto msg_torque = std_msgs::Float64();
         msg_torque.data = torque;
         pub_torque.publish(msg_torque);
     
-        double error = speed_actual - speed;
-        sum_error += error;
-        double delta_error = error-previous_error;
-        previous_error = error;
-        double cmd = (KP*error + KI*sum_error + KD*delta_error) / DIV;
-        if (cmd > 1) cmd = 1;
-        if (cmd <-1) cmd = -1;
-        double acceleration = (cmd<0)?-cmd:0;
-        double brake = (cmd>0)?cmd:0;
+        double speed_error = speed_actual - speed;
+        sum_error_speed += speed_error;
+        double speed_delta_error = speed_error-previous_error_speed;
+        previous_error_speed = speed_error;
+        double cmd = (SPEED_KP*speed_error + SPEED_KI*sum_error_speed + SPEED_KD*speed_delta_error) / SPEED_DIV;
+        cmd = -cmd;
+        if (cmd < -MAX_DECELARATION / KIA_SOUL_DECELERATION) cmd = -MAX_DECELARATION / KIA_SOUL_DECELERATION;
+        if (cmd >MAX_ACCELARATION / KIA_SOUL_ACCELERATION) cmd = MAX_ACCELARATION / KIA_SOUL_ACCELERATION;
+        double acceleration = (cmd>0)?cmd:0;
+        double brake = (cmd<0)?-cmd:0;
         printf("%lf, %lf, %lf, %lf \r\n",speed_actual ,speed , acceleration, brake);
 
         auto msg_accel = std_msgs::Float64();
